2ad01cd6b13af45c439b95c4a1175f6bedd55b2d
Florian Lichtenecker <florian.lichtenecker@gmail.com>
2023-08-16 15:53:01 +0200

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,6 @@ class Restaurant < ApplicationRecord
   validates :name, :address, :category, presence: true
   validates :category, inclusion: {
     in: %w[chinese italian japanese french belgian],
-    message: `%{value} is not a valid category`
+    message: '%{value} is not a valid category'
   }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_105450) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_122707) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
